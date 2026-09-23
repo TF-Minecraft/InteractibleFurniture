1a66da5d9bfc5b488810e041ddb1eace7536bcce
Ryan <7389646+ryanbarlow97@users.noreply.github.com>
2026-09-23 16:17:55 +0100

    build: use consolidated current jar inputs (#24)

--- .github/scripts/install-local-dependencies.sh
+++ .github/scripts/install-local-dependencies.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 sha256sum --check .github/dependencies.sha256
 
 mvn -B --no-transfer-progress org.apache.maven.plugins:maven-install-plugin:3.1.4:install-file \
-    -Dfile="libs/gson-2.10.1.jar" -DgroupId="local" -DartifactId="gson" \
-    -Dversion="2.10.1-tfmc-4241c14a7727" -Dpackaging=jar -DgeneratePom=true "$@"
+    -Dfile="libs/gson-2.14.0.jar" -DgroupId="local" -DartifactId="gson" \
+    -Dversion="2.14.0-tfmc-2cbd119bf196" -Dpackaging=jar -DgeneratePom=true "$@"
 mvn -B --no-transfer-progress org.apache.maven.plugins:maven-install-plugin:3.1.4:install-file \
-    -Dfile="libs/joml-1.10.8.jar" -DgroupId="local" -DartifactId="joml" \
-    -Dversion="1.10.8-tfmc-bf1951014517" -Dpackaging=jar -DgeneratePom=true "$@"
+    -Dfile="libs/joml-1.10.9.jar" -DgroupId="local" -DartifactId="joml" \
+    -Dversion="1.10.9-tfmc-feca4db85337" -Dpackaging=jar -DgeneratePom=true "$@"
